82d99bc1290bb6631f85caa895c8534f49035990
hayata <nunpoppoppo@gmail.com>
2020-11-25 12:05:10 +0900

    Add Employee Create

--- src/controllers/employees/EmployeesIndexServlet.java
+++ src/controllers/employees/EmployeesIndexServlet.java
@@ -14,16 +14,17 @@ import javax.servlet.http.HttpServletResponse;
 import models.Employee;
 import utils.DBUtil;
 
+
 @WebServlet("/employees/index")
 public class EmployeesIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    
+
     public EmployeesIndexServlet() {
         super();
     }
 
-    
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
@@ -52,4 +53,5 @@ public class EmployeesIndexServlet extends HttpServlet {
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/index.jsp");
         rd.forward(request, response);
     }
+
 }
